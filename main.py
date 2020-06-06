0d762b1530b5c065426b8d5cc1c2d4d21a6f5acc
KatyaPanukova <panyukowa-yekaterina@yandex.ru>
2020-06-06 18:41:04 +0300

    end 1

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ for i in Market.lst_products:
 print(Product.max_price())
 print(Product.total())
 Product.del_product('3851211')
-for i in Market.lst_products:
-    print(i)
+print(Product.add_product(Product('water', '04523312', '12.04.2020', '50')))
+print(Market.max_check())
+print(Market.return_check('Buyer 1', '06.06.2020', 15))
+print(Market.return_check('Buyer 2', '27.05.2020'))
--- solution.py
+++ solution.py
@@ -11,7 +11,9 @@ class Load:
                 basket = i.split(';')[1:]
                 Market.lst_products.append(Market(buyer, basket))
                 for item in basket:
-                    Product.lst_products.append(Product(item.split()))
+                    item = item.split()
+                    Product.lst_products.append(Product(item[0], item[1], item[2], item[3]))
+
 
 
 class Market:
@@ -24,6 +26,65 @@ class Market:
         self.buyer = buyer
         self.basket = basket
 
+    @classmethod
+    def max_check(cls):
+        """Max check."""
+        max_ = 0
+        buyer = ''
+        for i in cls.lst_products:
+            dol = 0
+            for j in i.basket:
+                j = j.split()[-1]
+                dol += int(j)
+            if max_ < dol:
+                buyer = str(i.buyer)
+                max_ = dol
+
+        return 'Client: {} purchased the goods in the amount {}.'.format(buyer, max_)
+
+    @classmethod
+    def return_check(cls, buyer, date, sale=None):
+        """"""
+        date_new = Date(date)
+        total = 0
+        result = ''
+        for i in cls.lst_products:
+            if str(i.buyer) == buyer:
+                for j in i.basket:
+                    total += int(j.split()[-1])
+        if sale is not None and type(sale) == int:
+            total_s = total - (total*int(sale)/100)
+            result += '-' * 60 + '\n'
+            result += '-' * 18 + 'Market "FAMILY_MARKET"' + '-' * 20 + '\n'
+            result += 'Cash payment' + '\n'
+            for item in cls.lst_products:
+                if str(item.buyer) == buyer:
+                    for j in item.basket:
+                        tol = j.split()
+                        result += str(Product(tol[0], tol[1], tol[2], tol[3]))
+            result += '-' * 60 + '\n'
+            result += 'Total without sale: {}\n'.format(total)
+            result += 'Total with sale {} % : {} \n'.format(sale, total_s)
+            result += 'Date: {} \n'.format(date_new)
+            result += 'Thank you for the shopping!!!' + '\n'
+            result += '-' * 60 + '\n'
+        elif sale is None:
+            result += '-'*60 + '\n'
+            result += '-'*18 + 'Market "FAMILY_MARKET"' + '-'*20 + '\n'
+            result += 'Cash payment' + '\n'
+            for item in cls.lst_products:
+                if str(item.buyer) == buyer:
+                    for j in item.basket:
+                        tol = j.split()
+                        result += str(Product(tol[0], tol[1], tol[2], tol[3]))
+            result += '-' * 60 + '\n'
+            result += 'Total: {} \n'.format(total)
+            result += 'Date: {} \n'.format(date_new)
+            result += 'Thank you for the shopping!!!' + '\n'
+            result += '-'*60 + '\n'
+
+        return result
+
     def __str__(self):
         """String output method."""
 
@@ -31,7 +92,7 @@ class Market:
         result += '{}'.format(self.buyer) + '\n'
         for item in self.basket:
             item = item.split()
-            result += str(Product(item))
+            result += str(Product(item[0], item[1], item[2], item[3]))
         result += '\n'
 
         return result
@@ -47,16 +108,22 @@ class Product:
 
     lst_products = []
 
-    def __init__(self, item):
+    def __init__(self, name, code, date, price):
         """Initialization method."""
-        self.name = item[0]
-        self.code = item[1]
-        self.date = item[2]
-        self.price = item[3]
+        self.name = name
+        self.code = code
+        self.date = date
+        self.price = price
 
         dictionary = {'385': 'Switzerland', '460': 'Russia', '400': 'German', '009': 'USA', '045': 'Japan'}
 
         self.country = dictionary[self.code[:3]]
+
+    @classmethod
+    def add_product(cls, new):
+        """Add new product in basket."""
+        cls.lst_products.append(new)
+        return 'Product add in basket.'
 
     @staticmethod
     def max_price():
@@ -103,3 +170,84 @@ class Product:
     def __repr__(self):
         """Output method."""
         return self.__str__()
+
+class Date:
+    """"""
+    date = None
+
+    def __init__(self, __date):
+        """"""
+        self.__date = str(__date)
+        self.dict_month = {'01': 'янв', '02': 'фев', '03': 'мар', '04': 'апр', '05': 'май', '06': 'июн',
+                           '07': 'июл', '08': 'авг', '09': 'сен', '10': 'окт', '11': 'ноя', '12': 'дек'}
+        self.dict_month_1 = {'01': 31, '02': 29, '03': 31, '04': 30, '05': 31, '06': 30,
+                             '07': 31, '08': 31, '09': 30, '10': 31, '11': 30, '12': 31}
+        self.dict_month_2 = {'01': 31, '02': 28, '03': 31, '04': 30, '05': 31, '06': 30,
+                        '07': 31, '08': 31, '09': 30, '10': 31, '11': 30, '12': 31}
+        new_date = self.__date.split('.')
+
+        if len(self.__date) != 10:
+            if 0 > int(new_date[0]) > 31 or new_date[1] not in self.dict_month:
+                print('ошибка')
+                self.__date = None
+
+
+    @property
+    def date(self):
+        """"""
+        if self.__date is None:
+            print('ошибка')
+            return self.__date
+        else:
+            return self.__date
+
+    @date.setter
+    def date(self, new):
+        """"""
+        if new is not None:
+            new_date = str(new).split('.')
+            year = int(new_date[2])
+            if len(new_date) == 10:
+                if 0 > int(new_date[0]) > 31 or new_date[1] not in self.dict_month:
+                    self.__date = None
+                elif year % 4 != 0:
+                    self.dict_month_1 = self.dict_month_2
+                    if new_date[1] in self.dict_month and int(new_date[0]) <= self.dict_month_1[new_date[1]]:
+                        self.__date = None
+                else:
+                    self.__date = new
+            else:
+                print('ошибка')
+                self.__date = None
+        else:
+            self.__date = None
+
+    @date.getter
+    def date(self):
+        """"""
+        if self.__date is None:
+            return None
+        elif self.__date is not None:
+            result = ''
+            if isinstance(self.__date, str):
+                list_inf = self.__date.split('.')
+                n = 0
+                for i in list_inf:
+                    if n == 0:
+                        day = i
+                        if day[0] == 0:
+                            day = day[1]
+                        day = int(day)
+                        result += str(day) + ' '
+                    if n == 1:
+                        month = i
+                        result += self.dict_month[month] + ' '
+                    if n == 2:
+                        year = str(int(i))
+                        result += year + ' г.'
+                    n += 1
+                return result
+
+    def __repr__(self):
+        """"""
+        return str(self.date)
